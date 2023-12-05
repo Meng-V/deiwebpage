5fbf9e663787dbe7f8646bc08f5e8b371e145ded
Meng-MiamiOH <43998638+Meng-MiamiOH@users.noreply.github.com>
2023-12-05 18:52:39 -0500

    Update layout.tsx

--- app/(blank)/layout.tsx
+++ app/(blank)/layout.tsx
@@ -1,13 +1,13 @@
 import "./style.css";
 
 export default function DashboardLayout({
-    children, // will be a page or nested layout
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <section>
-        {children}
-      </section>
-    )
-  }
+  children, // will be a page or nested layout
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
