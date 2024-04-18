8c49bbc7bbd8dacae66ddca4e4303cc23fd9753a
Meng-V <qum@miamioh.edu>
2024-04-18 12:36:33 -0400

    Update page.tsx

--- app/deicommittee/page.tsx
+++ app/deicommittee/page.tsx
@@ -88,25 +88,11 @@ function Clients() {
     <Container className="mt-24 sm:mt-32 lg:mt-40">
       <FadeIn>
         <h2>
-        Committee Information
+        {/* Committee Information */}
         </h2>
       </FadeIn>
       <FadeInStagger className="mt-10" faster>
         <Border as={FadeIn} />
-        <ul
-          role="list"
-          className="grid grid-cols-2 gap-x-8 gap-y-12 sm:grid-cols-3 lg:grid-cols-4"
-        >
-          {clients.map(([client, logo]) => (
-            <li key={client} className="group">
-              <FadeIn className="overflow-hidden">
-                <Border className="pt-12 group-[&:nth-child(-n+2)]:-mt-px sm:group-[&:nth-child(3)]:-mt-px lg:group-[&:nth-child(4)]:-mt-px">
-                  <Image src={logo} alt={client} unoptimized />
-                </Border>
-              </FadeIn>
-            </li>
-          ))}
-        </ul>
       </FadeInStagger>
     </Container>
   )
